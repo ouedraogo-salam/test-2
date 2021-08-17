2d415aaa6f3bc7699257c5f6a28b49965b93f1a5
adbSalam <84749142+ouedraogo-salam@users.noreply.github.com>
2021-08-17 11:25:11 +0000

    Add files via upload

---
+++ grades.js
@@ -0,0 +1,7 @@
+function gradingStudents() {
+    // Write your code here
+    let grades = [4, 73, 67, 38, 33];
+    let newGrades = grades.map(function (currentGrade) {
+        console.log(currentGrade);
+    });
+}
